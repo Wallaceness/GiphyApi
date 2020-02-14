f68fcc6e9d35508273cb88212e468ef3136c8db0
Wallaceness <ncw232@gmail.com>
2020-02-14 10:11:01 -0500

    removed the file

--- app/src/main/java/com/example/android/giphyapi/View/APIKey.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.giphyapi.View;
-
-public class APIKey {
-    private static String Key="s4rUU3v7MsyekuqBlKStgCz6X2FKqAZZ";
-
-    public static String getKey(){return APIKey.Key;}
-}
